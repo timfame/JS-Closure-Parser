1a838111475628204c57be077fb3fe3dcfb2e2c3
timfame <43498119+timfame@users.noreply.github.com>
2019-05-21 23:51:31 +0300

    Update Simple.java

--- src/test/manual/Simple.java
+++ src/test/manual/Simple.java
@@ -5,7 +5,6 @@ import main.TextParser;
 import main.exceptions.ClosureException;
 import main.exceptions.UndefinedException;
 import main.source.FileSource;
-import test.auto.Checker;
 
 import javax.script.ScriptException;
 import java.io.IOException;
@@ -13,15 +12,9 @@ import java.io.IOException;
 public class Simple {
 
     public static void main(String[] args) throws IOException, ClosureException, UndefinedException, ScriptException {
-      //  try (FileSource files = new FileSource(args[0], args[1])) {
-             FileSource files = new FileSource("src/test/manual/input.txt",
-                     "src/test/manual/output.txt");
-             Parser parser = new Parser(files);
-            System.out.println(parser.parse());
-        //    System.out.println(files.nextSymbol() + "" + news.get() + "" + files.nextSymbol() + "" + news.get());
-
-      //  } catch (Exception e) {
-      //      e.printStackTrace();
-      //  }
+        FileSource files = new FileSource("src/test/manual/input.txt",
+                "src/test/manual/output.txt");
+        Parser parser = new Parser(files);
+        System.out.println(parser.parse());
     }
 }
